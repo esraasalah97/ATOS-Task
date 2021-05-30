ad48ba9c07d7f79cab39cc296190c40986635147
esraasalah97 <46899002+esraasalah97@users.noreply.github.com>
2021-05-30 22:43:29 +0200

    fix parameters issue

--- task/src/test/java/tests/PhpTravelsSignUp.java
+++ task/src/test/java/tests/PhpTravelsSignUp.java
@@ -47,20 +47,20 @@ public class PhpTravelsSignUp {
 		// Check if parameter passed from TestNG is 'firefox'
 		if (browser.equalsIgnoreCase("firefox")) {
 			// create firefox instance
-			System.setProperty("webdriver.gecko.driver", "src//test//java//resources.drivers//geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "src//test//java//drivers//geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
 		// Check if parameter passed as 'chrome'
 		else if (browser.equalsIgnoreCase("chrome")) {
 			// set path to chromedriver.exe
-			System.setProperty("webdriver.chrome.driver", "src//test//java//resources.drivers//chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "src//test//java//drivers//chromedriver.exe");
 			// create chrome instance
 			driver = new ChromeDriver();
 		}
 		// Check if parameter passed as 'Edge'
 		else if (browser.equalsIgnoreCase("ie")) {
 			// set path to Edge.exe
-			System.setProperty("webdriver.ie.driver", "src//test//java//resources.drivers//IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver", "src//test//java//drivers//IEDriverServer.exe");
 			// create Edge instance
 			driver = new InternetExplorerDriver();
 		} else {
